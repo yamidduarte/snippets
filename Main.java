deb8b974a912b5018ecb9455e70e92c4009c2596
Yamid Duarte <104658177+yamidduarte@users.noreply.github.com>
2022-04-29 16:27:35 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,21 @@
+package com.yamidduarte;
+
+/*
+ 4.29.22 - Was asked a new question:
+ "Given a string of lowercase characters from ‘a’ – ‘z’,
+ write a solution to print the characters
+ of a string in sorted order.
+ Examples:
+ Input : bbccdefbbaa
+ Output : aabbbbccdef"
+  
+*/
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Solution test = new Solution();
+        test.printSorted("zyxwsfedcba");
+    }
+}
