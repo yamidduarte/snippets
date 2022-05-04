9d60b7acf6260bcd835ea1f51eb46c80711f4e93
Yamid Duarte <104658177+yamidduarte@users.noreply.github.com>
2022-05-04 09:29:18 -0700

    Create Main.java

---
+++ stacks/minimum-remove-to-make-valid-parentheses/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  
+  public static void main(String[] args) {
+    String testCase0 = "b(o)a(t()(d(a)ys";
+    Medium testSuite0 = new Medium();
+    System.out.println(testSuite0.evaluate(testCase0));    
+  }
+
+}
