ea6e1e9c3b5c329eb4f43fd5f606077ed49798d9
Yamid Duarte <104658177+yamidduarte@users.noreply.github.com>
2022-05-04 09:20:45 -0700

    Create Main.java

---
+++ stacks/stack-via-deque/Main.java
@@ -0,0 +1,27 @@
+import java.util.Deque;
+
+public class DequeExample {
+  public static void main(String[] args) {
+    Deque<Character> deque = ArrayDeque<>();
+
+    // Queue mode
+    deque.offerLast('a');
+    deque.offerLast('b');
+
+    String a = deque.pollFirst();
+    String b = deque.pollFirst();
+
+    // Stack mode
+    deque.offerFirst('a');
+    deque.offerFirst('b');
+
+    String a = deque.pollFirst();
+    String b = deque.pollFirst();
+
+    // Implementation example
+
+  
+  
+  }
+
+}
