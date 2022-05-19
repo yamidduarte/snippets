144a5f3b7a9d03be38d36f7c7c2e2bfd57913fa7
Yamid Duarte <104658177+yamidduarte@users.noreply.github.com>
2022-05-19 09:12:53 -0700

    Create fib.js

---
+++ dynamic/fib.js
@@ -0,0 +1,12 @@
+// n = some array?
+
+const fib = (n) => {
+    if(n <= 2) return 1;
+
+    return fib(n - 1) + fib(n - 2); 
+
+};
+
+console.log(fib(6));
+console.log(fib(7));
+console.log(fib(8));
