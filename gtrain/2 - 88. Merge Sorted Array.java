dc2f5ae6ac92256fd1944e794d82795d1f1560fc
Yamid Duarte <104658177+yamidduarte@users.noreply.github.com>
2022-05-27 17:02:58 -0700

    Create 2 - 88. Merge Sorted Array.java

---
+++ gtrain/2 - 88. Merge Sorted Array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        
+        var p1 = m - 1;
+        var p2 = n - 1;
+        var i = m + n - 1;
+        
+        while (p2 >= 0) {
+            
+            if (p1 >= 0 && nums1[p1] > nums2[p2])
+                nums1[i--] = nums1[p1--];
+            else
+                nums1[i--] = nums2[p2--];
+            
+        } 
+    }
+}
